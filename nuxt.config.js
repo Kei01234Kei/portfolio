2d1b227fd4f7e7431aca93830ef83ba7707c0d89
Keisuke Ishigami <keisuke.130@icloud.com>
2021-05-04 15:06:29 +0900

    テーマカラーを変更

--- nuxt.config.js
+++ nuxt.config.js
@@ -45,16 +45,16 @@ export default {
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: true,
+      light: true,
       themes: {
-        dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3
+        light: {
+          primary: "#e91e63",
+          secondary: "#00bcd4",
+          accent: "#cddc39",
+          error: "#ff5722",
+          warning: "#f44336",
+          info: "#2196f3",
+          success: "#8bc34a"
         }
       }
     }
