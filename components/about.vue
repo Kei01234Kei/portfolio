602fb646ff6e9529dbe25fbf844eb0aae10100f8
Keisuke Ishigami <keisuke.130@icloud.com>
2021-05-27 02:02:22 +0900

    aboutページを作り始めた

--- components/about.vue
+++ components/about.vue
@@ -1,18 +1,18 @@
 <template>
-    <v-row justify="center" align="center" class="my-5 mx-3">
-        <v-container class="text-center">
-            <v-col cols="12">
+  <v-row justify="center" align="center" class="my-5 mx-3">
+    <v-container class="text-center">
+      <v-col cols="12">
         <h1 class="display-1 font-weight"><router-link to="/about">About</router-link></h1>
       </v-col>
-            <v-col>
-                <p>
-                    岐阜大学工学部電気電子・情報工学科情報コース2年の石上敬祐です。<br>
-                    国際大学対抗プログラミングコンテストやYahoo! JAPAN主催のハッカソンなど、さまざまなイベントに参加してプログラミング技術を磨いています。<br>
-                </p>
-            </v-col>
-            <v-avatar size="150">
-                <v-img :src="require('@/assets/img/avatar.JPG')"></v-img>
-            </v-avatar>
-        </v-container>
-    </v-row>
+      <v-col>
+        <p>
+          岐阜大学工学部電気電子・情報工学科情報コース2年の石上敬祐です。<br>
+          国際大学対抗プログラミングコンテストやYahoo! JAPAN主催のハッカソンなど、さまざまなイベントに参加してプログラミング技術を磨いています。<br>
+        </p>
+      </v-col>
+      <v-avatar size="150">
+        <v-img :src="require('@/assets/img/avatar.JPG')"></v-img>
+      </v-avatar>
+    </v-container>
+  </v-row>
 </template>
--- pages/about.vue
+++ pages/about.vue
@@ -1,4 +1,15 @@
-<template><h1>about</h1></template>
+<template>
+<v-container>
+    <v-row class="my-5 mx-3">
+        <v-col cols="12">
+            <h1 class="display-1 font-weight">About</h1>
+        </v-col>
+        <v-col cols="12">
+            <p>岐阜大学にて情報工学を専攻しています。得意分野はweb系です。ReactやVueなどのフロントエンドフレームワークやNuxtなどのwebアプリケーションフレームワーク、AWSなどのクラウド技術等、さまざまな経験を有しています。</p>
+        </v-col>
+    </v-row>
+</v-container>
+</template>
 
 <script>
 </script>
